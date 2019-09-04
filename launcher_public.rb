c35f940621f789d177d589df09c61ef238293280
coffinzm <54777127+coffinzm@users.noreply.github.com>
2019-09-03 21:17:47 -0500

    Update launcher_public.rb

--- launcher_public.rb
+++ launcher_public.rb
@@ -4,7 +4,7 @@ require "rufus-scheduler"
 scheduler = Rufus::Scheduler.new	
 
 scheduler.every("2m")do
-	require "/home/pi/Bots/mtgbotpublic.rb"
+	load "/home/pi/Bots/mtgbotpublic.rb"
 end
 
 scheduler.join
